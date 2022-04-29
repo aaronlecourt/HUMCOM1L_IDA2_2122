be9a4dcceb2ce4e28e41be1a7bc28966299bfcaa
unknown <20217375@s.ubaguio.edu>
2022-04-29 18:52:13 +0800

    updatecover

---
+++ FINALEXAM/javascript/finalexam.js
@@ -0,0 +1,40 @@
+function backgroundone() {
+    document.getElementById("clickPlanet").style.background = "linear-gradient(rgba(0, 0, 0, 0.4), rgba(0, 0, 0, 0.4)),url('https://images.ctfassets.net/1gqwxa4vbed9/4HIWGCBkdL46XrSNu3AzHD/548da3a9ca6c4d67467124903b4446f6/short_stories.jpg')no-repeat";
+    document.getElementById("clickPlanet").style.backgroundSize = "cover";
+}
+function backgroundtwo() {
+    document.getElementById("clickPlanet").style.background = "linear-gradient(rgba(0, 0, 0, 0.4), rgba(0, 0, 0, 0.4)),url('https://images.ctfassets.net/7lhcm73ukv5p/4aO41OX3cxAzGcPN4HGKcE/dbaf443f34a59d5a2ebe06b11a01ac32/www-bottom-bg.jpg?w=2400&q=75&fm=webp')no-repeat";
+    document.getElementById("clickPlanet").style.backgroundSize = "cover";
+}
+function backgroundthree() {
+    document.getElementById("clickPlanet").style.background = "linear-gradient(rgba(0, 0, 0, 0.4), rgba(0, 0, 0, 0.4)),url('https://images.ctfassets.net/7lhcm73ukv5p/4aLtrYyDO4rpaFhV3qeWbp/87f5240c36d5a10fea0f64cc67d89b79/www-cta.jpg?w=2400&q=75&fm=webp')no-repeat";
+    document.getElementById("clickPlanet").style.backgroundSize = "cover";
+}
+function backgroundfour() {
+    document.getElementById("clickPlanet").style.background = "linear-gradient(rgba(0, 0, 0, 0.4), rgba(0, 0, 0, 0.4)),url('https://www.denofgeek.com/wp-content/uploads/2019/05/eve-online-amarr-fleet.png?fit=1920%2C1080')no-repeat";
+    document.getElementById("clickPlanet").style.backgroundSize = "cover";
+}
+function backgroundfive() {
+    document.getElementById("clickPlanet").style.background = "linear-gradient(rgba(0, 0, 0, 0.4), rgba(0, 0, 0, 0.4)),url('https://images8.alphacoders.com/385/385854.png')no-repeat";
+    document.getElementById("clickPlanet").style.backgroundSize = "cover";
+}
+function backgroundsix() {
+    document.getElementById("clickPlanet").style.background = "linear-gradient(rgba(0, 0, 0, 0.4), rgba(0, 0, 0, 0.4)),url('https://wallpaper-mania.com/wp-content/uploads/2018/09/High_resolution_wallpaper_background_ID_77700842093.jpg')no-repeat";
+    document.getElementById("clickPlanet").style.backgroundSize = "cover";
+}
+function backgroundseven() {
+    document.getElementById("clickPlanet").style.background = "linear-gradient(rgba(0, 0, 0, 0.4), rgba(0, 0, 0, 0.4)),url('https://crossingzebras.com/wp-content/uploads/2014/08/Wormhole_and_Drake.jpg')no-repeat";
+    document.getElementById("clickPlanet").style.backgroundSize = "cover";
+}
+function backgroundeight() {
+    document.getElementById("clickPlanet").style.background = "linear-gradient(rgba(0, 0, 0, 0.4), rgba(0, 0, 0, 0.4)),url('https://crossingzebras.com/wp-content/uploads/2017/01/eve-ascension-frontpageH.png')no-repeat";
+    document.getElementById("clickPlanet").style.backgroundSize = "cover";
+}
+function backgroundnine() {
+    document.getElementById("clickPlanet").style.background = "linear-gradient(rgba(0, 0, 0, 0.4), rgba(0, 0, 0, 0.4)),url('https://www.wallpaperup.com/uploads/wallpapers/2014/01/11/224211/944c812d5493ff78e4ef234f98caa15f.jpg')no-repeat";
+    document.getElementById("clickPlanet").style.backgroundSize = "cover";
+}
+function backgroundten() {
+    document.getElementById("clickPlanet").style.background = "linear-gradient(rgba(0, 0, 0, 0.4), rgba(0, 0, 0, 0.4)),url('https://external-preview.redd.it/qAwbx-Dh0j7_teu0sKGYBIZhmIyMSKElwiqtfvdE7Uo.jpg?auto=webp&s=7df8f26d9f91dd62a5e1a15dbafb0fd71789c389')no-repeat";
+    document.getElementById("clickPlanet").style.backgroundSize = "cover";
+}
