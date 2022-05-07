d3da88d80cb1018c38908041a17fbf1067f4e5da
unknown <20217375@s.ubaguio.edu>
2022-05-07 19:27:26 +0800

    add exercise 15

---
+++ javascript/Script15.js
@@ -0,0 +1,11 @@
+function displayAssignedValues(){
+    var firstName = document.getElementById("firstName").value = "Aaron Zayke Lecourt";
+    var age = document.getElementById("age").value = "20";
+    var phoneNumber = document.getElementById("phoneNumber").value = "0906 074 7979";
+    var eMail = document.getElementById("eMail").value = "20217375@s.ubaguio.edu";
+    var mailAddress = document.getElementById("mailAddress").value = "Assumption Rd., Baguio City";
+    var program = document.getElementById("program").value = "Computer Science";
+    var idNum = document.getElementById("idNum").value = "20217375";
+    var yearLevel = document.getElementById("yearLevel").value = "1st Year";
+    var section = document.getElementById("section").value = "IDA2";
+}
